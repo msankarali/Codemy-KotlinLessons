f7e32f38880ce74be33cea0ac002bfeecaaee688
Gökhan ÖZTÜRK <gokhanozturk@androidedu.io>
2020-01-16 19:38:32 +0300

    First commit with <3

---
+++ src/gettingstarted/1-PackageDefinitionAndImports.kt
@@ -0,0 +1,12 @@
+/**
+ *      package -   src klasorunden sonraki klasor hiyerarsisini gosterir.
+ *      import  -   kullandiginiz dile ait ya da harici kutuphaneleri belirtir.
+ */
+
+package gettingstarted
+
+import kotlin.math.pow
+
+fun main() {
+    3.14.pow(2.1)
+}
---
+++ src/gettingstarted/2-Program-Entry-Point.kt
@@ -0,0 +1,18 @@
+package gettingstarted
+
+/**
+ *      jvm projelerinin giris noktasi main fonksiyonudur.
+ */
+fun main() {
+    println("Codemy | Code Academy ~ The education you've always dreamed of")
+}
+
+/* -------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ *      @param args ile gerekiyorsa baslangic anina sabit parametrelerinizi gonderebilirsiniz.
+ *      Edit Configurations.. Configuration: Tab -> Program Arguments: -> icerisine parametreleri girebilirsiniz.
+ */
+//fun main(args: Array<String>) {
+//
+//}
